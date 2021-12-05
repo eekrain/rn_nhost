01707c130d78b37718ebb7f27bfd43132cedc74e
eekrain <ardianoption10@gmail.com>
2021-12-05 19:52:50 +0700

    success rename

--- android/app/src/main/java/com/rocketjaket/kasir/MainActivity.java
+++ android/app/src/main/java/com/rocketjaket/kasir/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rn_nhost;
+package com.rocketjaket.kasir;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "rn_nhost";
+    return "RocketjaketKasir";
   }
 }
--- ios/RocketjaketKasirTests/RocketjaketKasirTests.m
+++ ios/RocketjaketKasirTests/RocketjaketKasirTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rn_nhostTests : XCTestCase
+@interface RocketjaketKasirTests : XCTestCase
 
 @end
 
-@implementation rn_nhostTests
+@implementation RocketjaketKasirTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
